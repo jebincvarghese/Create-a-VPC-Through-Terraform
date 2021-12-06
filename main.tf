7015631904ef6409af239f9e3595998135d42794
jebincvarghese <43873109+jebincvarghese@users.noreply.github.com>
2021-12-06 21:29:33 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+data "aws_availability_zones" "az" {
+    
+  state = "available"
+
+}
+
+
+output "az-1" {
+  value = data.aws_availability_zones.az.names[0]
+}
+
+output "az-2" {
+  value = data.aws_availability_zones.az.names[1]
+}
+
+
+output "az-3" {
+  value = data.aws_availability_zones.az.names[2]
+}
